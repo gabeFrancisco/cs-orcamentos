70b634d5a6ad6a84e053605f51952ccf215813d0
gabefrancisco <gabegit2@gmail.com>
2026-09-22 15:06:33 -0300

    setado tamanho da pagina A4

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ function App() {
   return (
     <div className="flex flex-row bg-slate-300 w-dvw h-dvh">
       <div className="w-1/6 bg-white p-5 border-r border-slate-50 shadow">Chaveiro Sul Orçamentos</div>
-      <div className="w-full flex flex-col p-5 items-center">
-        <div className="bg-white rounded p-4">
+      <div className="w-full overflow-y-scroll  flex flex-col p-5 items-center">
+        <div className="bg-white rounded p-4 page">
 
         </div>
       </div>
