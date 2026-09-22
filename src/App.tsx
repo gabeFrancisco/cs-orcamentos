62755a794406462639b6dabc554da953f6c56382
gabefrancisco <gabegit2@gmail.com>
2026-09-22 15:00:24 -0300

    feito layout basico

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 function App() {
   return (
-    <div className="bg-blue-500 text-white">
-      Olá!
+    <div className="flex flex-row bg-slate-300 w-dvw h-dvh">
+      <div className="w-1/6 bg-white p-5 border-r border-slate-50 shadow">Chaveiro Sul Orçamentos</div>
+      <div className="w-full flex flex-col p-5 items-center">
+        <div className="bg-white rounded p-4">
+
+        </div>
+      </div>
     </div>
   )
 
