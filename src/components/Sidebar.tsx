5dc2ec97714d325fad7e88d00c8caf8531d4f5bf
gabefrancisco <gabegit2@gmail.com>
2026-09-24 22:28:26 -0300

    fixado bug de margin e padding no css global

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,21 @@
 function Sidebar() {
     return (
-        <div className="w-1/2 flex flex-col items-center bg-white text-zinc-700 rounded m-10 shadow-lg shadow-zinc-400">
+        <div className="w-1/2 flex flex-col bg-white text-zinc-700 rounded shadow-lg shadow-zinc-400">
             <div className="p-3 bg-blue-500 border-b text-white text-center border-zinc-300 shadow-lg w-full rounded-tl rounded-tr">
                 Gerenciador de Orçamentos
+            </div>
+            <div className="p-4 flex flex-col items-start">
+                <div className="w-full mt-3">
+                    <label htmlFor="remetente" className="txt-label">Remetente</label>
+                    <input name="remetente" type="text" className="txt-input" />
+                </div>
+            </div>
+            <hr className="text-zinc-300" />
+            <div className="p-4">
+                <label className="form-label">Itens</label>
+                <div className="form-section">
+
+                </div>
             </div>
         </div>
     );
