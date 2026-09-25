7cf661bd8992dd1e59d038f5ca231c5ff8ea3ce2
gabefrancisco <gabegit2@gmail.com>
2026-09-24 22:47:10 -0300

    iniciado estrutura do form de adição de itens

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,20 +1,40 @@
 function Sidebar() {
     return (
-        <div className="w-1/2 flex flex-col bg-white text-zinc-700 rounded shadow-lg shadow-zinc-400">
+        <div className="lg:w-3/5 2xl:w-1/3 flex flex-col bg-white text-zinc-700 rounded shadow-lg shadow-zinc-400">
             <div className="p-3 bg-zinc-800 border-b text-white text-center border-zinc-300 shadow-lg w-full rounded-tl rounded-tr">
                 Gerenciador de Orçamentos
             </div>
-            <div className="p-4 flex flex-col items-start">
-                <div className="w-full mt-3">
-                    <label htmlFor="remetente" className="txt-label">Remetente</label>
-                    <input name="remetente" type="text" className="txt-input" />
-                </div>
-            </div>
-            <hr className="text-zinc-300" />
-            <div className="p-4">
-                <label className="form-label">Itens</label>
-                <div className="form-section">
+            <div className="p-3">
 
+                <div className="flex flex-col items-start mt-1">
+                    <div className="w-full mt-3">
+                        <label htmlFor="remetente" className="txt-label">Remetente</label>
+                        <input name="remetente" type="text" className="txt-input" />
+                    </div>
+                </div>
+
+                <div className="mt-5">
+                    <label className="form-label">Itens</label>
+                    <div className="form-section">
+                        <div className="mt-3">
+                            <label htmlFor="nome" className="txt-label">Nome</label>
+                            <input type="text" name="nome" className="txt-input" />
+                        </div>
+
+
+                        <div className="mt-3 flex flex-row justify-between">
+                            <div>
+                                <label htmlFor="quantidade" className="txt-label">Qte.</label>
+                                <input type="number" name="quantidade" className="txt-input" />
+                            </div>
+                            <div className="ml-3">
+                                <label htmlFor="preco" className="txt-label">Preço</label>
+                                <input type="number" name="preco" className="txt-input" />
+                            </div>
+                        </div>
+
+                        <button type="button" className="mt-3 w-full btn btn-primary">Adicionar</button>
+                    </div>
                 </div>
             </div>
         </div>
